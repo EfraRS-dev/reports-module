25414398f803055f0f305909b5ed90bc262bc665
sandrotg <tsandro@uninorte.edu.co>
2025-08-18 21:32:04 -0500

    header terminado y conexion prisma
    header con botones y menu desplegable terminado y adicionalmente el backend ya tiene la conexión a prisma.

--- frontend/src/elements/dashboard.tsx
+++ frontend/src/elements/dashboard.tsx
@@ -1,6 +1,7 @@
 import Graphics from "./graphics";
 import "../styles/dashboard.css";
 import TablaReportes from "./table";
+import ReportHeader from "./reportHeader";
 
 const registros = [
   { id: 1, producto: "Laptop", ventas: 15000, fecha: "2025-08-01" },
@@ -10,6 +11,9 @@ const registros = [
 export default function Dashboard(){
     return(
         <div className="dashboard-container">
+            <header>
+                <ReportHeader />
+            </header>
             <section >
                 <Graphics />
             </section>
